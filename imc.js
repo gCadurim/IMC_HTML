37c899e6fc2db878011033275499988e8f9f24d1
gCadurim <49075372+gCadurim@users.noreply.github.com>
2021-07-24 20:31:04 -0300

    Create imc.js

---
+++ imc.js
@@ -0,0 +1,18 @@
+function calcular(peso, altura){
+    var peso = document.getElementById('txtPeso').value;
+    var altura = document.getElementById('txtAltura').value;
+    var imc = peso / (altura * altura);
+    document.getElementById('resultado').innerHTML = imc.toFixed(2);
+    
+    if(imc < 18.5){
+        document.getElementById('magreza').classList.add('destaque');
+    } else if(imc <= 24,9){
+        document.getElementById('normal').classList.add('destaque');
+    } else if(imc <= 30){
+        document.getElementById('sobrepeso').classList.add('destaque');
+    } else if(imc >= 30){
+        document.getElementById('obesidade').classList.add('destaque');
+    } else{
+        document.getElementById('resultado').innerHTML = 'digite um valor válido';
+    }
+}
